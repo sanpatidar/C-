979295876f9822bb92fc574b5cd1fb7230cb2857
Sanskar Patidar <149409060+sanpatidar@users.noreply.github.com>
2024-08-27 12:43:43 +0530

    Add files via upload

---
+++ Manipulators.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<left;
+    cout<<setw(10)<<"Name"<<setw(10)<<"Marks"<<endl;
+    cout<<setw(10)<<"Sanskar"<<setw(10)<<"93"<<endl;
+    cout<<setw(10)<<"Virat"<<setw(10)<<"98"<<endl;
+    cout<<setw(10)<<"Rohit"<<setw(10)<<"96"<<endl;
+    cout<<setw(10)<<"KL Rahul"<<setw(10)<<"95"<<endl;
+    return 0;
+}
