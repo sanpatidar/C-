4339fef236e6c1d760a0e64c47e45774153ad239
Sanskar Patidar <149409060+sanpatidar@users.noreply.github.com>
2024-08-30 11:39:53 +0530

    Add files via upload

---
+++ callbuyaddress.cpp
@@ -0,0 +1,20 @@
+//Call by address
+#include<bits/stdc++.h>
+using namespace std;
+
+void swap(int *x, int *y){
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+    
+}
+
+int main(){
+    int a = 10, b = 20;
+    cout <<"Before Swapping: "<<endl;
+    cout <<" a = "<<a<<" b = "<<b<<endl;
+    swap(&a,&b);
+    cout <<"After Swapping: "<<endl;
+    cout <<" a = "<<a<<" b = "<<b<<endl;
+}
